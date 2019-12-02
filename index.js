5a77b1264d5b949b58f109cfc4a5c702d752c297
Nathan Alex <nate745@gmail.com>
2019-12-02 11:15:00 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*"){
+    return function(x = "special"){
+        return `You are ${flair}${x}${flair}!`
+    }
+}
+
+let Calculator = {
+    add: () => 1+3,
+    subtract: () => 1-3,
+    multiply: () => 1*3,
+    divide: () => 10/5
+}
+
+function actionApplyer(startingInt, functArray) {
+    if (functArray.length === 0) {
+        return startingInt
+    } else {
+        let num = startingInt
+        functArray.forEach (funct => num = funct(num))
+        return num
+    }
+}
